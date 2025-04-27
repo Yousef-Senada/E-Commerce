0bb3c722126d4fceabd451b5d3b30bf20a3613ef
yousef-senada <yousef.a.senada@gmail.com>
2025-04-27 06:40:05 +0300

    pre-v1

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -6,5 +6,5 @@ export const environment = {
 if (environment.dev) {
     environment.apiUrl = 'http://localhost:3000/';
 } else {
-    environment.apiUrl = 'https://e-commerce-api-ruddy.vercel.app/';
+    environment.apiUrl = 'https://e-commerce-purfit-backend.vercel.app/';
 }
